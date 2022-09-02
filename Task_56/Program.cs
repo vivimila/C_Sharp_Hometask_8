766bf39db6ecfe6c98c2c78e822b0970e7cc2202
vivimila <violamilashevskaya@yandex.by>
2022-09-02 15:00:18 +0300

    Task 56 is solved

--- Task_56/Program.cs
+++ Task_56/Program.cs
@@ -9,3 +9,59 @@
 
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void SearchMinSum(int[,] matrix)
+{
+    int minsum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.WriteLine($"Сумма {i + 1}-й строки = {sum}");
+         if (sum == 0)
+        {
+             minsum = sum;
+        }
+         else if (sum < minsum)
+        {
+            minsum = sum;
+        }
+            
+    }
+    Console.WriteLine($"Сумма минимальной строкой = {minsum}");
+}
+int[,] Array2D = CreateMatrixRndInt(3, 4, -20, 10);
+PrintMatrix(Array2D);
+SearchMinSum(Array2D);
