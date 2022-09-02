b37e943464b9a65c7682942dda7b76e49114e45b
vivimila <violamilashevskaya@yandex.by>
2022-09-02 17:01:50 +0300

    work with a folder task 54

--- Task_54/Program.cs
+++ Task_54/Program.cs
@@ -8,3 +8,60 @@
 // 9 5 3 2
 // 8 4 4 2
 
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+//Console.WriteLine("");
+
+void PrintMatrixReverse(int[,] matrix)
+{
+    int maxIndex = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+        {
+            if (maxIndex > i) i--;
+            else i ++;
+        }
+    }
+
+}
+
+int[,] Array2D = CreateMatrixRndInt(3, 4, 1, 40);
+PrintMatrix(Array2D);
+Console.WriteLine("");
+PrintMatrixReverse(Array2D);
