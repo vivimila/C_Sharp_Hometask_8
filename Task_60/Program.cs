b6594f5c5b7a6d93173c909a9193272f8ed9a7e6
vivimila <violamilashevskaya@yandex.by>
2022-09-03 19:22:14 +0300

    Task 56 is solved without coordinate

--- Task_60/Program.cs
+++ Task_60/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] CreateMatrix (int row, int col, int dep, int min, int max) 
+{
+    int[,,] matrix = new int[row, col, dep];
+    Random rnd = new Random();   
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+                {
+                    matrix[i,j,k] = rnd.Next(min, max + 1);
+                }
+        }
+    }
+    return matrix;
+} 
+ 
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i <  matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+            Console.Write($"{matrix[i, j, k], 1}| ");            
+            }          
+        }   
+         Console.WriteLine();       
+    }        
+}
+
+Console.WriteLine();  
+int[,,] array3D = CreateMatrix(2, 2, 2, 10, 20);
+PrintMatrix(array3D);
