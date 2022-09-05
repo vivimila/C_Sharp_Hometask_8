e2b5107e7c2de2f4e3a5fa44b356a2c38721cf76
vivimila <violamilashevskaya@yandex.by>
2022-09-05 13:40:43 +0300

    Task 60 is solved

--- Task_60/Program.cs
+++ Task_60/Program.cs
@@ -1,4 +1,5 @@
-﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
 // 66(0,0,0) 25(0,1,0)
 // 34(1,0,0) 41(1,1,0)
@@ -6,39 +7,54 @@
 // 26(1,0,1) 55(1,1,1)
 
 
-int[,,] CreateMatrix (int row, int col, int dep, int min, int max) 
+int[,,] CreateMatrix(int row, int col, int dep, int min, int max)
 {
     int[,,] matrix = new int[row, col, dep];
-    Random rnd = new Random();   
-    for (int i = 0; i < matrix.GetLength(0); i++) 
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
             for (int k = 0; k < matrix.GetLength(2); k++)
-                {
-                    matrix[i,j,k] = rnd.Next(min, max + 1);
-                }
+            {
+                matrix[i, j, k] = rnd.Next(min, max + 1);
+            }
         }
     }
     return matrix;
-} 
- 
+}
+
 void PrintMatrix(int[,,] matrix)
 {
-    for (int i = 0; i <  matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        Console.Write("|");
-        for (int j = 0; j < matrix.GetLength(1); j++) 
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
             for (int k = 0; k < matrix.GetLength(2); k++)
             {
-            Console.Write($"{matrix[i, j, k], 1}| ");            
-            }          
-        }   
-         Console.WriteLine();       
-    }        
+            //Console.Write($"{matrix[i, j, k], 1}");            
+            }
+        }
+    }
 }
 
-Console.WriteLine();  
+void PrintMatrixIdex(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write(matrix[i, j, k]);
+                Console.Write("({0},{1},{2}) ", i, j, k);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
 int[,,] array3D = CreateMatrix(2, 2, 2, 10, 20);
 PrintMatrix(array3D);
+Console.WriteLine();
+PrintMatrixIdex(array3D);
